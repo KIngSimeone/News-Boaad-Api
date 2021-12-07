d56b802181cc719a6611fb84bdac0238ff63e15b
Simeon <simeoneumoh@outlook.com>
2021-12-07 09:08:32 +0100

    PostsView get

--- news_board/posts/views.py
+++ news_board/posts/views.py
@@ -8,3 +8,27 @@ from rest_framework.views import APIView
 from .serializers import CommentSerializer, PostSerializer
 from .utils import (create_post, delete_post, get_all_posts, get_post_by_id,
                     update_post)
+
+
+class PostsView(APIView):
+    pagination_class = CustomPagination
+
+    def get(self, request, format=None):
+        """Retrieve all posts"""
+
+        searchQuery = dict()
+        queryDict = request.query_params
+        filterQuery = queryDict.copy()
+        if queryDict:
+            if 'q' in filterQuery:
+                searchQuery = filterQuery.get('q')
+                del filterQuery['q']
+        
+        posts = get_all_posts(searchQuery, filterQuery)
+        serializer = PostSerializer(posts, many=True)
+        return http_response(
+            msg="Posts Retrieved Successfully",
+            status=status.HTTP_200_OK,
+            data=serializer.data
+        )
+        
