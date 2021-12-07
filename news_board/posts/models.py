4ea45467c8f516628c0f60acecf4549c86dae64a
Simeon <simeoneumoh@outlook.com>
2021-12-07 06:12:01 +0100

    Posts models

--- news_board/posts/models.py
+++ news_board/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+class Posts(models.Model):
+    title = models.TextField(null=True)
+    link = models.TextField(null=True)
+    upvotes = models.DateField(null=True)
+    author_name = models.DateField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    
