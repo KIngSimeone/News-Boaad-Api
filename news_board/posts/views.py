ca838b2c6f7dd88b84c7da8793bca4c5a8b4898a
Simeon <simeoneumoh@outlook.com>
2021-12-07 06:57:24 +0100

    views imports

--- news_board/posts/views.py
+++ news_board/posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from api_utils.custom_pagination import CustomPagination
+from api_utils.error_codes import ErrorCodes
+from api_utils.views import http_response, validate_keys
+from django.conf import settings
+from rest_framework import status
+from rest_framework.views import APIView
 
-# Create your views here.
+from .utils import (
+    get_all_posts, get_post_by_id, create_post, update_post,
+    delete_post
+)
