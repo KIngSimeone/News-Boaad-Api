5239af9e38d889aa616b8c65bb68c9fc6c0b3966
Simeon <simeoneumoh@outlook.com>
2021-12-07 07:03:43 +0100

    CommentSerializer updates

---
+++ news_board/posts/serializers.py
@@ -0,0 +1,22 @@
+from django.db import models
+from rest_framework import fields, serializers
+from .models import Posts, Comments
+import logging
+logger = logging.getLogger(__name__)
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Posts
+        fields = [
+            'id', 'title', 'link', 'author_name', 
+            'upvotes', 'created_date'
+        ]
+        read_only_fields = ['id', 'created_date']
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comments
+        fields = [
+            'id', 'author_name', 'created_date'
+        ]
+        read_only_fields = ['id', 'created_date']
