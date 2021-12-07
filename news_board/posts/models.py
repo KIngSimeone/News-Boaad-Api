04ac0e64288f4ff60208c8988052c395691b8b0a
Simeon <simeoneumoh@outlook.com>
2021-12-07 06:40:51 +0100

    Posts models update

--- news_board/posts/models.py
+++ news_board/posts/models.py
@@ -4,8 +4,8 @@ from django.utils.timezone import now
 class Posts(models.Model):
     title = models.TextField(null=True)
     link = models.TextField(null=True)
-    upvotes = models.DateField(null=True)
-    author_name = models.DateField(null=True)
+    upvotes = models.IntegerField(default=0)
+    author_name = models.TextField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
 class Comments(models.Model):
