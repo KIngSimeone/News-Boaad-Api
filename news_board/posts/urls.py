3106f9f918f823b0f948bbf3ab59da8228a51770
Simeon <simeoneumoh@outlook.com>
2021-12-08 07:17:26 +0100

    upvote

--- news_board/posts/urls.py
+++ news_board/posts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('/<int:id>', PostsView.as_view()),
     path('/comments/<int:id>', CommentsView.as_view()),
     path('/comments', CommentsView.as_view()),
-    path('upvote/<int:id>', upvote_post)
+    path('/upvote/<int:id>', upvote_post)
 ]
