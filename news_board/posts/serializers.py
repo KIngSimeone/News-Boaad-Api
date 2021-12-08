d4e5dc6f350d8cb7fc470cfc0e3a1b8fbc98c5f1
Simeon <simeoneumoh@outlook.com>
2021-12-08 06:43:26 +0100

    Comments serializer updates

--- news_board/posts/serializers.py
+++ news_board/posts/serializers.py
@@ -17,6 +17,6 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comments
         fields = [
-            'id', 'author_name', 'created_date'
+            'id','post', 'author_name', 'created_date'
         ]
         read_only_fields = ['id', 'created_date']
