6b747e43c110a4c1e55a1df3a0bea2abd2cb2917
Simeon <simeoneumoh@outlook.com>
2021-12-08 06:43:12 +0100

    get comments

--- news_board/posts/views.py
+++ news_board/posts/views.py
@@ -152,3 +152,154 @@ class PostsView(APIView):
                 "status": True
             }
         )
+
+
+class CommentsView(APIView):
+    pagination_class = CustomPagination
+
+    def get(self, request, postId, format=None):
+        """Retrieve Posts comments"""
+
+        post = get_post_by_id(postId)
+        if not post:
+            return http_response(
+                msg="Post not found",
+                status=status.HTTP_404_NOT_FOUND,
+                error_code=ErrorCodes.NOT_FOUND,
+            )
+
+        searchQuery = dict()
+        queryDict = request.query_params
+        filterQuery = queryDict.copy()
+        if queryDict:
+            if 'q' in filterQuery:
+                searchQuery = filterQuery.get('q')
+                del filterQuery['q']
+
+        comments = get_posts_comments(post, searchQuery, filterQuery)
+        serializer = CommentSerializer(comments, many=True)
+        return http_response(
+            msg="Comments Retrieved Successfully",
+            status=status.HTTP_200_OK,
+            data=serializer.data
+        )
+
+    def post(self, request, format=None):
+        """Create a comment"""
+
+        payload = request.data
+        # check if required fields is present
+        missingKeys = validate_keys(payload=payload, requiredKeys=[
+            'title', 'link', 'author_name',
+        ])
+        if missingKeys:
+            return http_response(
+                msg=f"The following key(s) are missing in the request payload: {missingKeys}",
+                status=status.HTTP_400_BAD_REQUEST,
+                error_code=ErrorCodes.INVALID_PAYLOAD,
+            )
+
+        title = payload['title']
+        link = payload['link']
+        author_name = payload['author_name']
+
+        created_post, msg = create_post(title, link, author_name)
+        if not created_post:
+            return http_response(
+                msg=msg,
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                error_code=ErrorCodes.SERVER_ERROR
+            )
+
+        serializer = PostSerializer(created_post)
+
+        return http_response(
+            msg="User Successfully Created",
+            status=status.HTTP_201_CREATED,
+            data=serializer.data
+        )
+
+    def put(self, request, id, format=None):
+        "Update a Post"
+
+        payload = request.data
+        # check if required fields is present
+        missingKeys = validate_keys(payload=payload, requiredKeys=[
+            'title', 'link', 'author_name',
+        ])
+        if missingKeys:
+            return http_response(
+                msg=f"The following key(s) are missing in the request payload: {missingKeys}",
+                status=status.HTTP_400_BAD_REQUEST,
+                error_code=ErrorCodes.INVALID_PAYLOAD,
+            )
+
+        post_to_update = get_post_by_id(id)
+        if not post_to_update:
+            return http_response(
+                msg="Post not found",
+                status=status.HTTP_404_NOT_FOUND,
+                error_code=ErrorCodes.NOT_FOUND,
+            )
+
+        title = payload['title']
+        link = payload['link']
+        author_name = payload['author_name']
+
+        updated_post, msg = update_post(
+            post_to_update, title, link, author_name)
+        if not updated_post:
+            return http_response(
+                msg=msg,
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                error_code=ErrorCodes.SERVER_ERROR
+            )
+
+        serializer = PostSerializer(updated_post)
+        return http_response(
+            msg="Post Successfully Updated",
+            status=status.HTTP_200_OK,
+            data=serializer.data
+        )
+
+    def get(self, request, id, format=None):
+        "Get single post"
+        post = get_post_by_id(id)
+        if not post:
+            return http_response(
+                msg="Post not found",
+                status=status.HTTP_404_NOT_FOUND,
+                error_code=ErrorCodes.NOT_FOUND,
+            )
+        serializer = PostSerializer(post)
+        return http_response(
+            msg="Post Successfully Retrieved",
+            status=status.HTTP_200_OK,
+            data=serializer.data
+        )
+
+    def delete(self, request, id, format=None):
+        "Get single post"
+        post = get_post_by_id(id)
+        if not post:
+            return http_response(
+                msg="Post not found",
+                status=status.HTTP_404_NOT_FOUND,
+                error_code=ErrorCodes.NOT_FOUND,
+            )
+        
+        deleted_post, msg = delete_post(post)
+        if not deleted_post:
+            return http_response(
+                msg=msg,
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                error_code=ErrorCodes.SERVER_ERROR
+            )
+
+        return http_response(
+            msg="Post Successfully Deleted",
+            status=status.HTTP_200_OK,
+            data={
+                "status": True
+            }
+        )
