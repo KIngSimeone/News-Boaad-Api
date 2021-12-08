3c924e933acd523a453139e2be40b84362e48e5e
Simeon <simeoneumoh@outlook.com>
2021-12-08 06:43:33 +0100

    CommentsView urls

--- news_board/posts/urls.py
+++ news_board/posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PostsView
+from .views import PostsView, CommentsView
 
 
 urlpatterns = [
     path('', PostsView.as_view()),
     path('/<int:id>', PostsView.as_view()),
+    path('/<int:postId>', CommentsView.as_view()),
 ]
